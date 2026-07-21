0744b1da45ed9fcccb10596233b9ffb46fef1446
daviClimaco <davibomfimclimaco@gmail.com>
2026-07-21 01:38:07 -0300

    feat : stardew repository for hair mod stardew valley

---
+++ ModEntry.cs
@@ -0,0 +1,12 @@
+using StardewModdingAPI;
+
+namespace MeuCabeloMod
+{
+    public class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            this.Monitor.Log("Meu Cabelo Mod carregado com sucesso!", LogLevel.Info);
+        }
+    }
+}
