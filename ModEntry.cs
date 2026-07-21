fbdcf40810ab7f2b6b5bafe95f72a694a3cb7911
daviClimaco <davibomfimclimaco@gmail.com>
2026-07-21 01:59:41 -0300

    feat : Data/HairData editing via C#

--- ModEntry.cs
+++ ModEntry.cs
@@ -1,4 +1,5 @@
 using StardewModdingAPI;
+using StardewModdingAPI.Events;
 
 namespace MeuCabeloMod
 {
@@ -7,6 +8,20 @@ namespace MeuCabeloMod
         public override void Entry(IModHelper helper)
         {
             this.Monitor.Log("Meu Cabelo Mod carregado com sucesso!", LogLevel.Info);
+            helper.Events.Content.AssetRequested += OnAssetRequested;
+        }
+
+        private void OnAssetRequested(object sender, AssetRequestedEventArgs e)
+        {
+            if (e.NameWithoutLocale.IsEquivalentTo("Data/HairData"))
+            {
+                e.Edit(asset =>
+                {
+                    var data = asset.AsDictionary<int, string>().Data;
+                    //bald hair example
+                    data[9001] = "Characters/Farmer/hairstyles/0/0/false/-1/false";
+                });
+            }
         }
     }
 }
